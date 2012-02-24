490eb1930630e8d1c64e07b6730da2a1d7e05d2f
Matthijs van Henten <mvhenten@tty.nl>
2012-02-24 17:44:25 +0100

    refactor company controller

--- common/controller/company.py
+++ common/controller/company.py
@@ -15,22 +15,19 @@ class Controller(BaseController):
             self.company = Company()
 
     def saveAction( self, key ):
-        company = self.company
-        properties = Company.properties()
-
-        for key, prop in properties.items():
+        for key, prop in Company.properties().items():
             value = self.request.get(key)
 
             if not value:
                 continue
-            
+
             if type( prop ).__name__ is 'IntegerProperty':
                 value = int(value)
 
             if type( prop ).__name__ is 'FloatProperty':
                 value = float(value)
 
-            company.__setattr__( key, value )
+            self.company.__setattr__( key, value )
 
         logo = self.request.get('logo')
 
@@ -38,10 +35,10 @@ class Controller(BaseController):
             src = images.Image(logo)
             src.horizontal_flip()
             src.horizontal_flip()
-            company.logo = src.execute_transforms(output_encoding=images.JPEG)
+            self.company.logo = src.execute_transforms(output_encoding=images.JPEG)
 
-        company.put()
-        self.redirect('/company/edit/%s' % company.key() )
+        self.company.put()
+        self.redirect('/company/edit/%s' % self.company.key() )
 
     def logoAction( self, key ):
         self.response.headers['Content-Type'] = 'image/jpg'
