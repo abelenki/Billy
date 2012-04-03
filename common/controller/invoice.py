45de9ccc384651f89f110caaaaa71ec459a84e81
Matthijs van Henten <matthijs@ischen.nl>
2012-04-04 00:01:54 +0200

    Added quick fix for pagination, removed generator links

--- common/controller/invoice.py
+++ common/controller/invoice.py
@@ -7,6 +7,62 @@ from common.models import *
 from common import render
 
 from base import BaseController
+import logging
+
+
+class GQLPager(object):
+
+    _prev_cursor = None
+    _next_cursor = None
+    _first_cursor = None
+    _results = None
+    _gql = None
+    _fetch_limit = 25
+
+    def __init__(
+        self,
+        gql,
+        current=None,
+        first=None,
+        ):
+        self._prev_cursor = current
+        self._first_cursor = first
+        self._gql = gql
+
+    def next(self):
+        self._fetch_and_init_cursor()
+        return self._next_cursor
+
+    def first(self):
+        if not self._first_cursor:
+            return self._prev_cursor
+
+        if self._prev_cursor == self._first_cursor:
+            return
+
+        return self._first_cursor
+
+    def prev(self):
+        if self._prev_cursor == self._first_cursor:
+            return
+
+        return self._prev_cursor
+
+    def fetch(self, limit=None):
+        self._fetch_limit = limit
+        self._fetch_and_init_cursor()
+        return self._results
+
+    def _fetch_and_init_cursor(self):
+        if self._results and self._next_cursor:
+            logging.debug(self._prev_cursor)
+            return
+
+        if self._prev_cursor:
+            self._gql.with_cursor(self._prev_cursor)
+
+        self._results = self._gql.fetch(self._fetch_limit)
+        self._next_cursor = self._gql.cursor()
 
 
 class Controller(BaseController):
@@ -170,9 +226,13 @@ class Controller(BaseController):
                 order by billed asc'
                             , account.key(), datetime.now() - timedelta(16))
 
+        pager = GQLPager(invoices, self.request.get('page'),
+                         self.request.get('first'))
+
+        self.template_values['invoices'] = pager.fetch(35)
         self.template_values['request'] = self.request
+        self.template_values['pager'] = pager
         self.template_values['tab'] = tab
-        self.template_values['invoices'] = invoices
 
     def deleteAction(self, key):
         invoice = Invoice.get(urllib.unquote(key))
